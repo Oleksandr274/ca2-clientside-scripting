6d197ec5b67a7597edf37ac2d8adf1f68ddb5253
Oleksandr274 <s00274217@atu.ie>
2024-12-08 21:07:36 +0000

    Fetching and displaying pizza data is complete

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 
-//fetch pizza information on openning details
+// fetch pizza information on openning details
 const pizzasDetails = document.getElementById('pizzas-details');
 pizzasDetails.addEventListener("toggle", () => {
     if (pizzasDetails.hasAttribute("open"))
         fetchPizzas(null);
 });
 
+
+// assign html elements to variables
 const pizzaBtnSearch = document.getElementById('btn-pizza-search');
 const pizzaSearchInput = document.getElementById('search-pizza');
-pizzaBtnSearch.addEventListener('click', () => {
-    console.log(pizzaSearchInput.value);
-    fetchPizzas(pizzaSearchInput.value);
-});
-
 const pizzaBtnFilter = document.getElementById('btn-pizza-filter');
 const pizzaVegTypeCheckbox = document.getElementById('veg-type-checkbox');
 const pizzaMinPriceInput = document.getElementById('pizza-min-price');
 
+
+// add event listener to search pizza button
+pizzaBtnSearch.addEventListener('click', () => {
+    // reset filter values to default
+    pizzaVegTypeCheckbox.checked = false;
+    pizzaMinPriceInput.value = 0;
+
+    console.log(pizzaSearchInput.value);
+    fetchPizzas(pizzaSearchInput.value);
+});
+
+// add event listener to filter pizza button
 pizzaBtnFilter.addEventListener('click', () => {
+    // reset search input to default
+    pizzaSearchInput.value = "";
+
     let isVeg = pizzaVegTypeCheckbox.checked;
-    let minPrice = pizzaMinPriceInput.value
+    let minPrice = Number(pizzaMinPriceInput.value)
     console.log(isVeg);
     console.log(minPrice);
     fetchPizzas(null, { isVeg, minPrice });
 });
 
-// Fetch and display all pizzas
+// Fetch and display pizzas
 async function fetchPizzas(pizzaName, filterOption) {
 
     // API URL and options
@@ -45,13 +57,20 @@ async function fetchPizzas(pizzaName, filterOption) {
         }
         const pizzas = await response.json();
 
-        console.log(pizzaName);
+        // display pizzas depending on user's request
         if (pizzaName != null) {
             const searchedPizzas = pizzas.filter(pizza => pizza.name.includes(pizzaName));
             displayPizzas(searchedPizzas);
         }
-        if (filterOption != null) {
-            const filteredPizzas = pizzas.filter(pizza => pizza.veg == filterOption.isVeg && pizza.price >= filterOption.minPrice)
+        else if (filterOption != null) {
+            let filteredPizzas = [];
+            if(!isNaN(filterOption.minPrice) && filterOption.minPrice > 0)
+                filteredPizzas = pizzas.filter(pizza => pizza.price >= filterOption.minPrice)
+            else
+                filteredPizzas = pizzas.filter(pizza => pizza.price >= 0)
+
+            if(filterOption.isVeg)
+                filteredPizzas = filteredPizzas.filter(pizza => pizza.veg == true );
             displayPizzas(filteredPizzas);
         }
         else {
@@ -135,75 +154,4 @@ function displayPizzas(pizzas) {
         //add pizza card to pizzas container
         pizzasContainer.appendChild(pizzaCard);
     })
-}
-
-function displayPizza(pizza) {
-    const pizzasContainer = document.getElementById('pizzas-container');
-    pizzasContainer.innerHTML = "";
-
-    //create pizza card element
-    const pizzaCard = document.createElement('div');
-    pizzaCard.classList.add("pizza-card");
-
-    //create pizza image 
-    const pizzaImage = document.createElement('img');
-    pizzaImage.setAttribute('src', pizza.img);
-    pizzaImage.setAttribute('alt', pizza.name);
-    pizzaImage.style.width = "100%";
-    pizzaImage.style.height = "auto";
-
-
-    //create pizza name
-    const pizzaNameDiv = document.createElement('div');
-    pizzaNameDiv.classList.add('pizza-name');
-    pizzaNameDiv.innerText = pizza.name;
-
-
-    //create pizza information
-    const pizzaInfoDiv = document.createElement('div');
-    pizzaInfoDiv.classList.add('pizza-info');
-
-    //create pizza description paragraph
-    const pizzaDescription = document.createElement('p');
-    pizzaDescription.classList.add('pizza-description');
-    pizzaDescription.innerText = pizza.description;
-
-    //create vegetarian type with bold label and yes/no value
-    const pizzaVegType = document.createElement('p');
-    const vegLabelSpan = document.createElement('span');
-    vegLabelSpan.innerText = "Vegetarian: ";
-    vegLabelSpan.style.fontWeight = "bold";
-    pizzaVegType.appendChild(vegLabelSpan);
-
-    const vegTypeParag = document.createElement('p');
-    vegTypeParag.style.display = "inline-block"
-    vegTypeParag.innerText = pizza.veg ? 'Yes' : 'No';
-    pizzaVegType.appendChild(vegTypeParag);
-
-    //create price with bold label and price value
-    const pizzaPrice = document.createElement('p');
-    const priceLabelSpan = document.createElement('span');
-    priceLabelSpan.innerText = "Price: ";
-    priceLabelSpan.style.fontWeight = "bold";
-    pizzaPrice.appendChild(priceLabelSpan);
-
-    const priceParag = document.createElement('p');
-    priceParag.innerText = '\u20AC' + pizza.price;
-    priceParag.style.display = "inline-block"
-    pizzaPrice.appendChild(priceParag);
-
-    //add description, vegetarian type, price to pizza info
-    pizzaInfoDiv.appendChild(pizzaDescription);
-    pizzaInfoDiv.appendChild(pizzaVegType);
-    pizzaInfoDiv.appendChild(pizzaPrice);
-
-
-    //add all elements to pizza card
-    pizzaCard.appendChild(pizzaImage);
-    pizzaCard.appendChild(pizzaNameDiv);
-    pizzaCard.appendChild(pizzaInfoDiv);
-
-
-    //add pizza card to pizzas container
-    pizzasContainer.appendChild(pizzaCard);
 }
