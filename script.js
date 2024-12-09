609224a173f0531629747b633c5b6bd2447084e4
Oleksandr274 <s00274217@atu.ie>
2024-12-09 00:11:50 +0000

    fetch/dispay desserts data added, css classes generalised

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+/* Pizza */
 
 // fetch pizza information on openning details
 const pizzasDetails = document.getElementById('pizzas-details');
 pizzasDetails.addEventListener("toggle", () => {
     if (pizzasDetails.hasAttribute("open"))
-        fetchPizzas(null);
+        fetchPizzas(null, null);
 });
 
 
@@ -22,7 +23,7 @@ pizzaBtnSearch.addEventListener('click', () => {
     pizzaMinPriceInput.value = 0;
 
     console.log(pizzaSearchInput.value);
-    fetchPizzas(pizzaSearchInput.value);
+    fetchPizzas(pizzaSearchInput.value, null);
 });
 
 // add event listener to filter pizza button
@@ -32,6 +33,10 @@ pizzaBtnFilter.addEventListener('click', () => {
 
     let isVeg = pizzaVegTypeCheckbox.checked;
     let minPrice = Number(pizzaMinPriceInput.value)
+    if(isNaN(minPrice) || minPrice < 0){
+        minPrice = 0;
+        pizzaMinPriceInput.value = 0;
+    }
     console.log(isVeg);
     console.log(minPrice);
     fetchPizzas(null, { isVeg, minPrice });
@@ -64,10 +69,8 @@ async function fetchPizzas(pizzaName, filterOption) {
         }
         else if (filterOption != null) {
             let filteredPizzas = [];
-            if(!isNaN(filterOption.minPrice) && filterOption.minPrice > 0)
-                filteredPizzas = pizzas.filter(pizza => pizza.price >= filterOption.minPrice)
-            else
-                filteredPizzas = pizzas.filter(pizza => pizza.price >= 0)
+
+            filteredPizzas = pizzas.filter(pizza => pizza.price >= filterOption.minPrice)
 
             if(filterOption.isVeg)
                 filteredPizzas = filteredPizzas.filter(pizza => pizza.veg == true );
@@ -90,7 +93,7 @@ function displayPizzas(pizzas) {
     pizzas.forEach(pizza => {
         //create pizza card element
         const pizzaCard = document.createElement('div');
-        pizzaCard.classList.add("pizza-card");
+        pizzaCard.classList.add("item-card");
 
         //create pizza image 
         const pizzaImage = document.createElement('img');
@@ -102,17 +105,16 @@ function displayPizzas(pizzas) {
 
         //create pizza name
         const pizzaNameDiv = document.createElement('div');
-        pizzaNameDiv.classList.add('pizza-name');
+        pizzaNameDiv.classList.add('item-name');
         pizzaNameDiv.innerText = pizza.name;
 
 
         //create pizza information
         const pizzaInfoDiv = document.createElement('div');
-        pizzaInfoDiv.classList.add('pizza-info');
+        pizzaInfoDiv.classList.add('item-info');
 
         //create pizza description paragraph
         const pizzaDescription = document.createElement('p');
-        pizzaDescription.classList.add('pizza-description');
         pizzaDescription.innerText = pizza.description;
 
         //create vegetarian type with bold label and yes/no value
@@ -153,5 +155,146 @@ function displayPizzas(pizzas) {
 
         //add pizza card to pizzas container
         pizzasContainer.appendChild(pizzaCard);
+    })
+}
+
+
+
+/* Desserts */
+
+// fetch pizza information on openning details
+const dessertsDetails = document.getElementById('desserts-details');
+dessertsDetails.addEventListener("toggle", () => {
+    if (dessertsDetails.hasAttribute("open"))
+        fetchDesserts(null, null);
+});
+
+
+// assign html elements to variables
+const dessertBtnSearch = document.getElementById('btn-dessert-search');
+const dessertSearchInput = document.getElementById('search-dessert');
+const dessertBtnFilter = document.getElementById('btn-dessert-filter');
+const dessertMinPriceInput = document.getElementById('dessert-min-price');
+
+
+// add event listener to search dessert button
+dessertBtnSearch.addEventListener('click', () => {
+    // reset filter values to default
+    dessertMinPriceInput.value = 0;
+
+    console.log(dessertSearchInput.value);
+    fetchDesserts(dessertSearchInput.value, null);
+});
+
+// add event listener to filter dessert button
+dessertBtnFilter.addEventListener('click', () => {
+    // reset search input to default
+    dessertSearchInput.value = "";
+
+    let minPrice = Number(dessertMinPriceInput.value)
+    if(isNaN(minPrice) || minPrice < 0){
+        minPrice = 0;
+        dessertMinPriceInput.value = 0;
+    }
+    console.log(minPrice);
+    fetchDesserts(null, minPrice );
+});
+
+// Fetch and display desserts
+async function fetchDesserts(dessertName, minPrice) {
+
+    // API URL and options
+    const url = 'https://pizza-and-desserts.p.rapidapi.com/desserts';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'c1a9fa6a9emsha7ec5f75b7bfd01p1c0300jsn1426eea4383e',
+            'x-rapidapi-host': 'pizza-and-desserts.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const desserts = await response.json();
+
+        // display desserts depending on user's request
+        if (dessertName != null) {
+            const searchedDesserts = desserts.filter(dessert => dessert.name.includes(dessertName));
+            displayDesserts(searchedDesserts);
+        }
+        else if (minPrice != null) {
+            const filteredDesserts = desserts.filter(dessert => dessert.price >= minPrice);
+            displayDesserts(filteredDesserts);
+        }
+        else {
+            displayDesserts(desserts);
+        }
+    }
+    catch (error) {
+        document.getElementById('desserts-container').textContent = `Error fetching desserts data: ${error.message}`;
+    }
+}
+
+// Function to display desserts
+function displayDesserts(desserts) {
+    const dessertsContainer = document.getElementById('desserts-container');
+    dessertsContainer.innerHTML = "";
+
+    desserts.forEach(dessert => {
+        //create dessert card element
+        const dessertCard = document.createElement('div');
+        dessertCard.classList.add("item-card");
+
+        //create dessert image 
+        const dessertImage = document.createElement('img');
+        dessertImage.setAttribute('src', dessert.img);
+        dessertImage.setAttribute('alt', dessert.name);
+        dessertImage.style.width = "100%";
+        dessertImage.style.height = "auto";
+
+
+        //create dessert name
+        const dessertNameDiv = document.createElement('div');
+        dessertNameDiv.classList.add('item-name');
+        dessertNameDiv.innerText = dessert.name;
+
+
+        //create dessert information
+        const dessertInfoDiv = document.createElement('div');
+        dessertInfoDiv.classList.add('item-info');
+
+        //create dessert description paragraph
+        const dessertDescription = document.createElement('p');
+        dessertDescription.innerText = dessert.description;
+
+
+        //create price with bold label and price value
+        const dessertPrice = document.createElement('p');
+        const priceLabelSpan = document.createElement('span');
+        priceLabelSpan.innerText = "Price: ";
+        priceLabelSpan.style.fontWeight = "bold";
+        dessertPrice.appendChild(priceLabelSpan);
+
+        const priceParag = document.createElement('p');
+        priceParag.innerText = '\u20AC' + dessert.price;
+        priceParag.style.display = "inline-block"
+        dessertPrice.appendChild(priceParag);
+
+        //add description, vegetarian type, price to dessert info
+        dessertInfoDiv.appendChild(dessertDescription);
+        dessertInfoDiv.appendChild(dessertPrice);
+
+
+        //add all elements to dessert card
+        dessertCard.appendChild(dessertImage);
+        dessertCard.appendChild(dessertNameDiv);
+        dessertCard.appendChild(dessertInfoDiv);
+
+
+        //add dessert card to desserts container
+        dessertsContainer.appendChild(dessertCard);
     })
 }
